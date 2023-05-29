daccd88e59333a89d865ed758a31a0ca26c2b0db
so2emma <osoemmanuel1969@gmail.com>
2023-05-29 23:37:20 +0100

    creating custom locale middleware

--- app/Http/Middleware/LocaleMiddleware.php
+++ app/Http/Middleware/LocaleMiddleware.php
@@ -4,6 +4,9 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class LocaleMiddleware
 {
@@ -16,6 +19,28 @@ class LocaleMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
+        $locale = null;
+        if (Auth::check() && !Session::has('locale'))
+        {
+            $locale = $request->user()->locale;
+            Session::put('locale', $locale);
+        }
+
+        if($request->has('locale'))
+        {
+            $locale = $request->get('locale');
+            Session::put('locale', $locale);
+        }
+
+        $locale = Session::get('locale');
+
+        if(null === $locale)
+        {
+            $locale = config('app.fallback_locale');
+        }
+
+        App::setLocale($locale);
+
         return $next($request);
     }
 }
